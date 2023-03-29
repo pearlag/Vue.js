95497214cb1fc6f352a09fcbf68ed46f7118d39a
jueun jin <74699797+pearlag@users.noreply.github.com>
2023-03-29 17:36:36 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<h2>input value</h2>
+		<input type="text" v-model="inputValue" />
+		<!-- <input
+			type="text"
+			:value="inputValue"
+			@input="event => (inputValue = event.target.value)"
+		/> -->
+		<div>{{ inputValue }}</div>
+
+		<h2>textarea value</h2>
+		<textarea :value="teatAreaValue"></textarea>
+		<div>{{ teatAreaValue }}</div>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const inputValue = ref('');
+		const teatAreaValue = ref('');
+		return { inputValue, teatAreaValue };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
