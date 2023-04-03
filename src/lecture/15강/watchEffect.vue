354cd5adf216115fd2fd1d4e4c993cd1bcde9535
jueun jin <74699797+pearlag@users.noreply.github.com>
2023-04-03 17:12:36 +0900

    watch, watchEffect

---
+++ src/lecture/15강/watchEffect.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<form @submit.prevent>
+			<input v-model.lazy="title" placeholder="title" type="text" /><br />
+			<textarea v-model.lazy="cont" placeholder="cont"></textarea>
+			<hr />
+			<button @click="save(title, cont)">저장</button>
+		</form>
+	</div>
+</template>
+
+<script>
+import { ref, watchEffect } from 'vue';
+
+export default {
+	setup() {
+		const title = ref('');
+		const cont = ref('');
+		const save = (title, cont) => {
+			console.log(`저장되었습니다. title: ${title}, cont: ${cont}`);
+		};
+
+		//데이터의 변경이 일어났을 때 콜백함수 호출됨.
+		watchEffect(() => {
+			console.log('watchEffect');
+			save(title.value, cont.value);
+		});
+		return { title, cont, save };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
