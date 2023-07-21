19cda7eeb306c65d5619e2903a2c01c8b4e10e54
jueun jin <74699797+pearlag@users.noreply.github.com>
2023-07-21 13:02:00 +0900

    study day13

---
+++ src/components/ProvideInject.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="container py-4">
+		<div class="card">
+			<div class="card-header">ProvideInject Component</div>
+			<div class="card-body">
+				<button @click="count++">click</button>
+				<Child></Child>
+				<p>{{ appMessage }}</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { provide, ref, readonly, inject } from 'vue';
+import Child from './Child.vue';
+export default {
+	components: { Child },
+	setup() {
+		const staticMessage = 'static message';
+		const message = ref('message');
+		const updateMessage = appendMessage => {
+			message.value = message.value + appendMessage;
+		};
+		const count = ref(10);
+
+		provide('static-message', staticMessage);
+		provide('message', { message: readonly(message), updateMessage });
+		provide('count', count);
+
+		const appMessage = inject('app-message');
+		return { count, appMessage };
+	},
+	mounted() {
+		console.log(this.msg);
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -1,68 +1,17 @@
 <template>
 	<main>
-		<div class="container py-4">
-			<MyButton class="my-button" id="my-button" @click="sayHello"></MyButton>
-			<LabelInput label="이름" data-id="이름"></LabelInput>
-			<hr />
-
-			<FancyButton>click!!<span style="color: red">@@</span></FancyButton>
-			<FancyButton #="{ fancyMessage }">
-				{{ fancyMessage }}
-			</FancyButton>
-
-			<FancyButton>
-				<template #="{ fancyMessage }">{{ fancyMessage }}</template>
-			</FancyButton>
-
-			<hr />
-			<AppCard>
-				<!-- <template #[slotArgs]>제목</template> -->
-				<!-- 이 부분은 암시적으로 default 슬롯으로 출력
-				<template #footer>푸터..</template> -->
-				<template #header="{ headerMessage }">
-					{{ headerMessage }}
-				</template>
-
-				<template #default="{ childMessage, helloMessage }">
-					디폴트입니다.
-					<br />
-					{{ message }}
-					<br />
-					{{ childMessage }}
-					<br />
-					{{ helloMessage }}
-				</template>
-
-				<template #footer="{ footerMessage }">
-					{{ footerMessage }}
-				</template>
-			</AppCard>
-			<hr />
-			<AppCard> 게시글입니다. </AppCard>
-		</div>
+		<ProvideInject></ProvideInject>
 	</main>
 </template>
 
 <script>
-import MyButton from './MyButton.vue';
-import LabelInput from './LabelInput.vue';
-import FancyButton from './FancyButton.vue';
-import AppCard from './AppCard.vue';
-import { ref } from 'vue';
+import ProvideInject from './ProvideInject.vue';
 export default {
 	components: {
-		MyButton,
-		LabelInput,
-		FancyButton,
-		AppCard,
+		ProvideInject,
 	},
 	setup() {
-		const sayHello = () => {
-			alert('안녕하세요.');
-		};
-		const slotArgs = ref('header');
-		const message = ref('hello');
-		return { sayHello, slotArgs, message };
+		return {};
 	},
 };
 </script>
